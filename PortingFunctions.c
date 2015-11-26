2a27f92f28392bcc9a0f9dfb5145e076a496ace4
Brad Love <b-rad@nextdimension.cc>
2015-11-26 12:44:25 -0500

    Fix another incorrect free / mark new to be free

--- PortingFunctions.c
+++ PortingFunctions.c
@@ -295,8 +295,7 @@ void GetMetaData(const char* path, struct CdsMediaObject *cdsObj)
 				char channel_nr[16] = { 0 };
 				channel_number(title, &channel_nr);
 
-				cdsObj->DeallocateThese &= CDS_ALLOC_Title;
-				free(cdsObj->Title);
+				cdsObj->DeallocateThese |= CDS_ALLOC_Title;
 				cdsObj->Title = (char*)malloc(strlen(cdsObj->ChannelNr) +
 				                              strlen(cdsObj->CallSign) +
 											  strlen(title) + 12);
