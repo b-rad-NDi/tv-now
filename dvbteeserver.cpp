9f7b58245c5c32ed5ffef74462cf1b1650a23e27
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-12-14 14:44:38 -0500

    tvnow/dvbteeserver: fix error: redeclaration of ‘dvbtee_context tmpContext’
    fixes #7

--- dvbteeserver.cpp
+++ dvbteeserver.cpp
@@ -653,9 +653,6 @@ extern "C" void dvbtee_start(void* nothing)
 #if TVNOW_TESTDATA
 	load_test_data();
 #else
-	dvbtee_context tmpContext;
-	context = &tmpContext;
-	context->server = NULL;
 
 #if 1 /* FIXME */
 	ATSCMultipleStringsInit();
