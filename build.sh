57813e4c33e80bcf37bef00b8bec9e27b24c1660
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-12-15 00:07:54 -0500

    build.sh: pass command line arguments to configure

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# To force disable HdHomeRun support, re-run this shell script with --disable-hdhr
+
 if [ ! -d libdvbtee ] ; then
     git clone git://github.com/mkrufky/libdvbtee.git
 fi
 
-(cd libdvbtee; ./build-auto.sh --prefix=`pwd`/usr/ --enable-static; make install)
-# To force disable HdHomeRun support, disable the above & enable the following:
-# (cd libdvbtee; ./build-auto.sh --prefix=`pwd`/usr/ --enable-static --disable-hdhr; make install)
+(cd libdvbtee; ./build-auto.sh --prefix=`pwd`/usr/ "$@" --enable-static; make install)
